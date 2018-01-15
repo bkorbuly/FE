2654305e7ab94dbda202fc5286d97c432b77bbd9
bkorbuly <b.korbuly@gmail.com>
2018-01-15 09:56:11 +0100

    exercise_done

--- FE/FE/Program.cs
+++ FE/FE/Program.cs
@@ -6,7 +6,29 @@ namespace FE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array1 = new int[] { 1, 2, 3 };
+            int[] array2 = new int[] { 6, 7, 8 };
+            int[] arrayMixed = Mix(array1, array2);
+            foreach(int i in arrayMixed)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine(); 
+        }
+
+        static int[] Mix(int[] array1, int[] array2)
+        {
+            int arrayLength = 2 * array1.Length;
+            int[] mixedArray = new int[arrayLength];
+            int k = -1;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                k++;
+                mixedArray[k] = array1[i];
+                k++;
+                mixedArray[k] = array2[i];
+            }            
+            return mixedArray;
         }
     }
 }
